f660ccd73fcc622aa9947dd94179616f5950f566
ibenyago <imene.benyagoub@etu.utc.fr>
2023-09-25 18:59:45 +0200

    SWF_SCF_majoSimple

--- ia04/comsoc/majo_simple.go
+++ ia04/comsoc/majo_simple.go
@@ -1,1 +1,33 @@
 package comsoc
+
+//SWF: retourne un décompte (count)
+//SCF: retourne les alternatives préférées
+
+func MajoritySWF(p Profile) (count Count, err error) {
+
+	err = checkProfile(p)
+	if err != nil {
+		return nil, err
+	}
+
+	for i := range p {
+		count[p[i][0]] += 1
+	}
+
+	return count, err
+
+}
+
+func MajoritySCF(p Profile) (bestAlts []Alternative, err error) {
+	err = checkProfile(p)
+	if err != nil {
+		return nil, err
+	}
+
+	for i := range p {
+		count[p[i][0]] += 1
+	}
+
+	return maxCount(count), err
+
+}

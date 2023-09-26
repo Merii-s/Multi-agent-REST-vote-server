08f80e5c4b92948cafa8cd907ede88efc09fa92d
Merii-s <massil.bouzar1@gmail.com>
2023-09-26 12:22:00 +0200

    fix utils commands

--- ia04/comsoc/utils.go
+++ ia04/comsoc/utils.go
@@ -1,5 +1,9 @@
 package comsoc
 
+import (
+	"errors"
+)
+
 // renvoie l'indice ou se trouve alt dans prefs
 func rank(alt Alternative, prefs []Alternative) int {
 	for i, v := range prefs {
@@ -18,11 +22,14 @@ func isPref(alt1, alt2 Alternative, prefs []Alternative) bool {
 // renvoie les meilleures alternatives pour un décompte donné
 func maxCount(count Count) (bestAlts []Alternative) {
 	for alt, nb := range count {
+		if len(bestAlts) == 0 {
+			bestAlts = append(bestAlts, alt)
+		}
 		for _, best := range bestAlts {
 			if nb > count[best] {
 				bestAlts = make([]Alternative, 0)
 				bestAlts = append(bestAlts, alt)
-			} else if nb == count[best] {
+			} else if nb == count[best] && alt != best {
 				bestAlts = append(bestAlts, alt)
 			}
 		}
@@ -31,7 +38,57 @@ func maxCount(count Count) (bestAlts []Alternative) {
 }
 
 // vérifie le profil donné, par ex. qu'ils sont tous complets et que chaque alternative n'apparaît qu'une seule fois par préférences
-func checkProfile(prefs Profile) error
+func checkProfile(prefs Profile) error {
+	verif := make(map[Alternative]int)
+	maxLen := 0
+	altUsed := make(map[Alternative]bool)
+
+	for _, pref := range prefs {
+		if len(pref) > maxLen {
+			maxLen = len(pref)
+		}
+		for _, alt := range pref {
+			if !altUsed[alt] {
+				altUsed[alt] = true
+			}
+		}
+	}
+
+	for _, pref := range prefs { // Pour chaque votant
+		for _, alt := range pref { // Pour chacune de ses prefs
+			verif[alt]++
+		}
+		if len(verif) != maxLen {
+			return errors.New("Pas même nombre d'alternatives")
+		} else {
+			for alt := range altUsed {
+				if verif[alt] != 1 {
+					return errors.New("Préférences incohérentes")
+				}
+			}
+		}
+		verif = make(map[Alternative]int) // Réinitialiser le map
+	}
+	return nil // Aucune erreur trouvée
+}
 
 // vérifie le profil donné, par ex. qu'ils sont tous complets et que chaque alternative de alts apparaît exactement une fois par préférences
-func checkProfileAlternative(prefs Profile, alts []Alternative) error
+func checkProfileAlternative(prefs Profile, alts []Alternative) error {
+	verif := make(map[Alternative]int)
+	for _, pref := range prefs { // Pour chaque votant
+		for _, alt := range pref { // Pour chacune de ses prefs
+			verif[alt]++
+		}
+		if len(verif) != len(pref) {
+			return errors.New("Pas même nombre d'alternatives")
+		} else {
+			for _, alt := range alts {
+				if verif[alt] != 1 {
+					return errors.New("Préférences incohérentes")
+				}
+			}
+		}
+		verif = make(map[Alternative]int) // Réinitialiser le map
+	}
+	return nil // Aucune erreur trouvée
+}

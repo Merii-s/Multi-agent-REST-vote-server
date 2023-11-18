e47b4095441faa37053a624e300c4e394decb482
Merii-s <massil.bouzar1@gmail.com>
2023-11-18 18:21:49 +0100

    fix tieBreak verification

--- ia04/agt/utils.go
+++ ia04/agt/utils.go
@@ -2,13 +2,26 @@ package agt
 
 func CheckTieBreak(nb_alts int, tieBreak []int) bool {
 
-	for i, val := range tieBreak {
+	verif := make(map[int]int)
+
+	// Alternative hors range
+	for _, val := range tieBreak {
 		if val > nb_alts || val <= 0 {
 			return true
 		}
-		for _, el := range tieBreak[i+1:] {
-			if val == el {
-				return true
+	}
+
+	for _, alt := range tieBreak { // Pour chacune de ses prefs
+		verif[alt]++
+	}
+	// Pas le même nombre d'alternatives
+	if len(verif) != nb_alts {
+		return true
+	} else {
+		// Chaque élément présent est bien entre 1 et nb_alts et apparaît une seule fois
+		for i := 1; i <= nb_alts; i++ {
+			if verif[i] != 1 {
+				return false
 			}
 		}
 	}
--- ia04/comsoc/utils.go
+++ ia04/comsoc/utils.go
@@ -106,17 +106,3 @@ func checkProfileAlternative(prefs Profile, alts []Alternative) error {
 	}
 	return nil // Aucune erreur trouvée
 }
-
-func checkTieBreak(nb_alts int, tieBreak []int) bool {
-	checks := make(map[int]bool)
-	for _, el := range tieBreak {
-		if el > nb_alts || el <= 0 {
-			return false
-		} else if checks[el] == false {
-			checks[el] = true
-		} else if checks[el] == true {
-			return false
-		}
-	}
-	return true
-}

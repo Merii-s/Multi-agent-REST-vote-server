81f11d1968f6bd463fc56525c5b802d43305ffd5
Massil Bouzar <massil.bouzar@etu.utc.fr>
2023-11-25 23:40:36 +0000

    Feature vote and result

--- ia04/agt/ballotagent.go
+++ ia04/agt/ballotagent.go
@@ -8,13 +8,22 @@ type RestBallotAgent struct {
 	id          string
 	rule        string
 	deadline    string
-	voter_ids   []string
+	voter_ids   map[string]bool // true si a voté
 	alts_number int
 	tie_break   []int
+	profile     [][]int
 }
 
-func NewRestBallotAgent(id string, rule string, deadline string, voter_ids []string, alts_number int, tie_break []int) *RestBallotAgent {
-	return &RestBallotAgent{id, rule, deadline, voter_ids, alts_number, tie_break}
+func NewRestBallotAgent(id string, rule string, deadline string, voter_ids map[string]bool, alts_number int, tie_break []int, profile [][]int) *RestBallotAgent {
+	return &RestBallotAgent{
+		id:          id,
+		rule:        rule,
+		deadline:    deadline,
+		voter_ids:   voter_ids,
+		alts_number: alts_number,
+		tie_break:   tie_break,
+		profile:     profile,
+	}
 }
 
 // Check deadline
